8fb1068d08e55d763a615f54b0f248cd838d1d99
elvis <elvisptavares@gmail.com>
2022-01-27 08:55:07 -0300

    created resource controller

---
+++ database/seeders/GameTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Game;
+use Illuminate\Database\Seeder;
+
+class GameTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
